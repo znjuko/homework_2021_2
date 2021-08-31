2bb85d411f67aa1bb4e3e79b74e86b467bb7c393
Черных Никита Александрович <nachernyk1@stream.ru>
2021-08-31 21:43:33 +0300

    js doc updated && code formatted

--- source/chess.js
+++ source/chess.js
@@ -4,23 +4,23 @@
 /**
  * Used for creating ASCII chessboard of size * size characters starting w/ '*'
  *
- * @param {null} size - chessboard size
+ * @param {number|string} size - chessboard size
  * @returns {string|null} - resulting chessboard
  */
 
 // using let instead of var, 'cause it's been outdated
 let chess = (size) => {
-  if (!(size >= 2 && Number.isInteger(+size))) {
-    return null;
-  }
-
-  let board = "";
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      board += (i + j) % 2 === 0 ? "*" : " ";
+    if (!(size >= 2 && Number.isInteger(+size))) {
+        return null;
     }
-    board += "\n";
-  }
 
-  return board;
+    let board = "";
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            board += (i + j) % 2 === 0 ? "*" : " ";
+        }
+        board += "\n";
+    }
+
+    return board;
 };
